4e37a85ea3d5d1722e64bf87e22898a45e51818d
Maxime PEREZ <54.maxime.perez@gmail.com>
2022-08-27 15:39:16 +0200

    part 4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 import shutil
 import json as module_json
+from collections import Counter
 
 from pd_options import *
 
@@ -26,11 +27,12 @@ def load_data() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     drugs_folder = "data/drugs/"
     publication_medical_folder = "data/pubmed/"
 
-    clinicals_trials_files = [clinicals_trials_folder + file for file in os.listdir(clinicals_trials_folder)]\
-        .remove(f"{clinicals_trials_folder}.gitkeep")
-    drugs_files = [drugs_folder + file for file in os.listdir(drugs_folder)].remove(f"{drugs_folder}.gitkeep")
-    publication_medical_files = [publication_medical_folder + file for file in os.listdir(publication_medical_folder)]\
-        .remove(f"{publication_medical_folder}.gitkeep")
+    clinicals_trials_files = [clinicals_trials_folder + file for file in os.listdir(clinicals_trials_folder)]
+    clinicals_trials_files.remove(f"{clinicals_trials_folder}.gitkeep")
+    drugs_files = [drugs_folder + file for file in os.listdir(drugs_folder)]
+    drugs_files.remove(f"{drugs_folder}.gitkeep")
+    publication_medical_files = [publication_medical_folder + file for file in os.listdir(publication_medical_folder)]
+    publication_medical_files.remove(f"{publication_medical_folder}.gitkeep")
 
     # on vérifie que les dossiers contenant les données ne sont pas vides
     assert clinicals_trials_files, exit("le dossier des tests clinique est vide")
@@ -45,7 +47,7 @@ def load_data() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     for idx, files in enumerate([clinicals_trials_files, drugs_files, publication_medical_files]):
         for file in files:
             if idx == 0:
-                df_clinicals_trials = df_clinicals_trials.append(pd.read_csv(file))
+                df_clinicals_trials = df_clinicals_trials.append(pd.read_csv(file,encoding="utf8"))
             elif idx == 1:
                 df_drugs = df_drugs.append(pd.read_csv(file))
             elif idx == 2:
@@ -85,8 +87,14 @@ def merge_csv(df_array) -> pd.DataFrame:
                       "id_y": "id_medical_publication", "title": "title_medical_publication",
                       "date_y": "date_medical_publication", "journal_y": "journal_medical_publication"}) \
         .drop(columns=["atccode_y"])
-
+    # met toutes les dates au même format
     df_merged["date_medical_publication"] = df_merged["date_medical_publication"].dt.strftime("%-d %B %Y")
+    # enlève les caractères non ascii
+    df_merged["journal_clinical_trial"] = df_merged["journal_clinical_trial"].astype("string")\
+        .replace(to_replace=r"\\xc3\\x28", value="", regex=True)
+
+    df_merged["title_clinical_trial"] = df_merged["title_clinical_trial"].astype("string")\
+        .replace(to_replace=r" \\xc3\\xb1", value="", regex=True)
 
     # on formate le df de telle sorte à faciliter la création de la structure du fichier json
     df_result = pd.DataFrame()
@@ -131,11 +139,11 @@ def csv_to_json(df) -> list:
 # fonction qui enregistre le fichier json
 # si production = False, enregistre aussi le json selon un format facilement lisible
 def save_json(json, timestamp, production=False):
-    with open(f'./output/production/drugs_informations_{timestamp}', 'w') as f:
+    with open(f'./output/production/drugs_informations_{timestamp}.json', 'w') as f:
         f.write(module_json.dumps(json))
     # format plus facile à lire
     if not production:
-        with open(f'./output/development/drugs_informations_{timestamp}', 'w') as f:
+        with open(f'./output/development/drugs_informations_{timestamp}.json', 'w') as f:
             f.write(module_json.dumps(json, indent=4))
 
 
@@ -160,6 +168,34 @@ def reverse_move_files():
                 shutil.move(f"data_processed/{folder}/{file}", f"data/{folder}/{'_'.join(start.split('_')[:-1])}.{end}")
 
 
+# récupère le dernier fichier json puis regarde quel est le journal qui mentionne le plus de médicaments différents
+def most_mentioned_journal():
+    output_folder = "output/production/"
+    files = [output_folder + file for file in os.listdir(output_folder)]
+    files.remove(f"{output_folder}.gitkeep")
+    # extrait les timestamps des noms des fichiers puis récupère l'index correspondant au plus grand timestamp
+    list_ts = list(map(lambda x: int(x.split("_")[-1].split(".")[0]), files))
+    idx_last_file = list_ts.index(max(list_ts))
+    # charge le fichier
+    json = module_json.load(open(files[idx_last_file]))
+    # map = performances
+    journals = list(map(lambda x: extract_journal(x), json))
+    # clean result
+    journals = [x for sublist in journals for x in sublist]
+    # affiche le journal le plus cité parmi les médicaments
+    # "Journal of photochemistry and photobiology. B, Biology" est le seul à être cité dans 2 médicaments différents
+    print(list(Counter(journals))[0])
+
+
+# récupère pour chaque médicament les journaux associés
+def extract_journal(json):
+    journal_medical = [medical["journal_medical_publication"] for medical in json["medical_publication"]]
+    journal_clinical = [clinical["journal_clinical_trial"] for clinical in json["clinical_trial"]]
+
+    return list(set(journal_medical + journal_clinical))
+
+
 if __name__ == "__main__":
-    #dag()
-    reverse_move_files()
+    # dag()
+    # reverse_move_files()
+    most_mentioned_journal()
